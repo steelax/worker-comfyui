327bc51503eebb08f0249f2f82d7ba6798cba32b
Steelax <steevhan@googlemail.com>
2025-06-28 16:45:48 +0100

    fixed the Main_Run_Sell location issue.

--- main_run.sh
+++ main_run.sh
@@ -8,27 +8,6 @@ export LD_PRELOAD="${TCMALLOC}"
 comfy-manager-set-mode offline || echo "worker-comfyui: Could not set ComfyUI-Manager network_mode" >&2
 
 
-echo "INITIALISATION worker-comfyui: Getting Latest Scripts from Network Store"
-wget -O /cert.json https://raw.githubusercontent.com/steelax/worker-comfyui/firebase/cert.json
-cp /cert.json /runpod_volume/sd/cert.json
-echo "INITIALISATIONworker-comfyui: handler.py updated"
-
-wget -O /rp_firebase_upload.py https://raw.githubusercontent.com/steelax/worker-comfyui/firebase/rp_firebase_upload.py
-cp /rp_firebase_upload.py /runpod_volume/sd/rp_firebase_upload.py
-echo "INITIALISATION -comfyui: rp_firebase_upload.py updated"
-
-echo "getting dirs"
-ls /
-
-wget -O /handler.py https://raw.githubusercontent.com/steelax/worker-comfyui/firebase/handler.py
-cp /handler.py /runpod_volume/sd/handler.py
-echo "INITIALISATION worker-comfyui: handler.py updated"
-echo "INITIALISATION worker-comfyui: COMPLETE: Getting Latest Scripts from Network Store"
-
-echo "INITIALISATION worker-comfyui: Copying Custom Nodes from Network Store"
-cp /runpod_volume/sd/custom_nodes/ / -r
-echo "INITIALISATIONworker-comfyui: COMPLETE: Copying Custom Nodes from Network Store"
-
 echo "INITIALISATION worker-comfyui: Starting ComfyUI"
 # Allow operators to tweak verbosity; default is DEBUG.
 : "${COMFY_LOG_LEVEL:=DEBUG}"
@@ -38,6 +17,28 @@ if [ "$SERVE_API_LOCALLY" == "true" ]; then
   echo "worker-comfyui: Starting RunPod Handler"
   python -u /handler.py --rp_serve_api --rp_api_host=0.0.0.0
 else
+
+  echo "INITIALISATION worker-comfyui: Getting Latest Scripts from Network Store"
+  wget -O /cert.json https://raw.githubusercontent.com/steelax/worker-comfyui/firebase/cert.json
+  cp /cert.json /runpod_volume/sd/cert.json
+  echo "INITIALISATION worker-comfyui: handler.py updated"
+
+  wget -O /rp_firebase_upload.py https://raw.githubusercontent.com/steelax/worker-comfyui/firebase/rp_firebase_upload.py
+  cp /rp_firebase_upload.py /runpod_volume/sd/rp_firebase_upload.py
+  echo "INITIALISATION -comfyui: rp_firebase_upload.py updated"
+
+  echo "getting dirs"
+  ls /runpod-volume
+
+  wget -O /handler.py https://raw.githubusercontent.com/steelax/worker-comfyui/firebase/handler.py
+  cp /handler.py /runpod_volume/sd/handler.py
+  echo "INITIALISATION worker-comfyui: handler.py updated"
+  echo "INITIALISATION worker-comfyui: COMPLETE: Getting Latest Scripts from Network Store"
+
+  echo "INITIALISATION worker-comfyui: Copying Custom Nodes from Network Store"
+  cp /runpod_volume/sd/custom_nodes/ / -r
+  echo "INITIALISATION worker-comfyui: COMPLETE: Copying Custom Nodes from Network Store"
+
   python -u /comfyui/main.py --disable-auto-launch --disable-metadata --verbose "${COMFY_LOG_LEVEL}" --log-stdout &
   echo "worker-comfyui: Starting RunPod Handler"
   python -u /handler.py
