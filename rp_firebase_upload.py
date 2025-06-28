0f4f1ae5c299b8f8fb617af4e19b24f7783f0462
Steelax <steevhan@googlemail.com>
2025-06-28 15:44:07 +0100

    amended firebase function logic

--- rp_firebase_upload.py
+++ rp_firebase_upload.py
@@ -1,25 +1,40 @@
-
-
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import storage
+import base64
 
 
+def file_to_base64(file_path):
+    """Converts the contents of a file to a Base64-encoded string."""
+    with open(file_path, 'rb') as file:
+        encoded_string = base64.b64encode(file.read()).decode('utf-8')
+    return encoded_string
 
-def upload_to_firebase(bucket, source_file_path, destination_blob_name):
-    """Uploads a file to the bucket."""
-    blob = bucket.blob(destination_blob_name)
+def upload_to_firebase(cert_file, bucket, data, destination_blob_name):
+    """Uploads a file to the bucket from Base64 string."""
 
-    blob.upload_from_filename(source_file_path)
-    blob.upload_from_string()
+    cred = credentials.Certificate(cert_file)
+    firebase_admin.initialize_app(cred, {
+        'storageBucket': bucket
+    })
 
-    print(f"File {source_file_path} uploaded to {destination_blob_name}.")
+    # Get the storage bucket
+    bucket = storage.bucket()
 
-cred = credentials.Certificate(r'C:\Users\steph\PycharmProjects\worker-comfyui\cert.json')
-firebase_admin.initialize_app(cred, {
-    'storageBucket': 'podlax-serverless-image-oylm5z.firebasestorage.app'
-})
+    try:
+        # Decode the Base64 string
+        image_data = base64.b64decode(data)
 
-bucket = storage.bucket()
+        # Create blob object
+        blob = bucket.blob(destination_blob_name)
 
-upload_to_firebase(bucket,r'C:\Users\steph\PycharmProjects\worker-comfyui\ABC Logo.png','Inputs/test.jpg')
+        # Upload binary data from string
+        blob.upload_from_string(image_data, content_type='image/jpeg')
+
+        print(f"Base64 string uploaded to {destination_blob_name}.")
+        return True
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return False
+
